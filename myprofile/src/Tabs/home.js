bae720e75f2de64679ef2490e488fb74c91554c7
Rucha Kulkarni <rucha2@pdx.edu>
2022-02-28 17:19:58 -0800

    Home Page

---
+++ myprofile/src/Tabs/home.js
@@ -0,0 +1,35 @@
+import "./home.css"
+export default function Home() {
+
+   return(
+    /*<div class="homeDiv">
+    <p class="normalText">Hi, My name is <br/>
+    <span class="Name">Rucha Kulkarni!</span>
+    <br/>I am a graduate student at Portland State University.
+    I am a software engineer and this is my personal 
+    website. You can find all my professional details here. 
+    <br/><br/>Please feel free to check my previous work and projects!</p>
+    </div>*/
+    <div>
+      <div class="homeDiv">
+         <div class="homeImage">
+            <img alt="Profile" src="./My_Pic0.jpeg" class="homePic"></img>
+         </div>
+         <div class="homeText">
+         <p class="normalText">Hi, My name is <br/>
+         <span class="Name">Rucha Kulkarni</span></p>
+         <p class="homeTextP">I am a graduate student at Portland State 
+         University pursuing master’s in computer science. I worked as a 
+         software developer and technology analyst in Information Technology 
+         industry for 6 years. My objective is to utilize my knowledge 
+         and skills to develop software solutions, build my career with accomplishments
+         and be valuable for society.<br/><br/>Please look around my website and if you think that 
+         my skills can add value to your company please feel free to reach out!</p>
+         </div>
+      </div>
+    </div>
+   )
+
+}
+
+/**<img src="./My_Pic1.jpeg" alt="Profile_Pic" class="profilePic"></img> */
