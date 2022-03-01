c9ced43ebd47f0915364f40e1df42b5831a8e4ca
Rucha Kulkarni <rucha2@pdx.edu>
2022-02-28 19:24:33 -0800

    About Page added

---
+++ myprofile/src/Tabs/about.js
@@ -0,0 +1,22 @@
+import "./about.css"
+export default function About() {
+
+   return(
+    <div>
+        <div class="aboutDiv">
+            <div class="aboutText">
+                <p class="aboutTextP">
+                I am a graduate student from Portland State University with a M.S. in Computer Science. 
+                I have had the opportunity to gain industry experience in software development where I’ve 
+                applied the skills I have learned in my previous degree. 
+                I like to code so in my free time, I enjoy solving analytical problems and 
+                learn new programming languages.</p>
+            </div>
+            <div class="aboutImage">
+                <img src="./My_Pic1.jpeg" alt="Profile_Pic" class="aboutPic"></img> 
+            </div>
+        </div>
+        
+    </div>
+   )
+}
