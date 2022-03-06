efdb3ee6425d3f404dc43ab72af836f9c3d14366
Rucha Kulkarni <rucha2@pdx.edu>
2022-03-05 23:32:15 -0800

    CSS changes for Navbar

--- myprofile/src/App.js
+++ myprofile/src/App.js
@@ -19,7 +19,7 @@ function App() {
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
         <ul class="navbar-nav">
               <li class="nav-item">
                 <Link to="/about"><h4 class="links">About</h4></Link>
