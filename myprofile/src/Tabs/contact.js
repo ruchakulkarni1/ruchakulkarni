61f68b0daf4f78ffb5145ba5be68fbd41f834d8d
Rucha Kulkarni <rucha2@pdx.edu>
2022-03-07 19:17:10 -0800

    Contact Page wave errors resolved

--- myprofile/src/Tabs/contact.js
+++ myprofile/src/Tabs/contact.js
@@ -10,15 +10,18 @@ export default function Contact() {
                 <h1 class="row formTitle">Contact me</h1>
                 <div class="row">
                    <div class="col">
-                        <input type="text" class="form-control form-control-lg" placeholder="Your Name" required/>
+                        <label for="name" class="labelContact">Name*</label>
+                        <input type="text" id="name" class="form-control form-control-lg" required/>
                     </div>
                     <div class="col">
-                        <input type="email" class="form-control form-control-lg" placeholder="Your E-Mail Address" required/>
+                        <label for="email" class="labelContact">Email*</label>
+                        <input type="email" id="email" class="form-control form-control-lg" required/>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-12">
-                        <textarea id="contact-textarea" class="form-control form-control-lg" placeholder="Your Message" rows="5" required></textarea>
+                        <label for="contact-textarea" class="labelContact">Your message</label>
+                        <textarea id="contact-textarea" class="form-control form-control-lg" rows="5" required></textarea>
                     </div>
                 </div>
                 <div class="row">
